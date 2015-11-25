a23e3b465efba3b8ea36bf25396df22fe873c153
Jacek Lewandowski <lewandowski.jacek@gmail.com>
2015-11-25 19:32:33 +0100

    improve-it-tests: Refactored SparkTemplate
    Allowing to stop SparkContext by providing null configuration.
    Set some Cassandra connection properties to be more suitable for testing.

--- spark-cassandra-connector-embedded/src/main/scala/com/datastax/spark/connector/embedded/SparkTemplate.scala
+++ spark-cassandra-connector-embedded/src/main/scala/com/datastax/spark/connector/embedded/SparkTemplate.scala
@@ -23,13 +23,18 @@ trait SparkTemplate {
 object SparkTemplate {
 
   /** Default configuration for [[org.apache.spark.SparkContext SparkContext]]. */
-  val defaultConf = new SparkConf(true)
+  val _defaultConf = new SparkConf(true)
     .set("spark.cassandra.connection.host", getHost(0).getHostAddress)
     .set("spark.cassandra.connection.port", getPort(0).toString)
+    .set("spark.cassandra.connection.keep_alive_ms", "5000")
+    .set("spark.cassandra.connection.timeout_ms", "30000")
+    .set("spark.cassandra.query.retry.delay", "1")
     .set("spark.ui.enabled", "false")
     .set("spark.cleaner.ttl", "3600")
     .setMaster(sys.env.getOrElse("IT_TEST_SPARK_MASTER", "local[*]"))
     .setAppName("Test")
+
+  def defaultConf = _defaultConf.clone()
 
   private var _sc: SparkContext = _
 
@@ -38,7 +43,13 @@ object SparkTemplate {
     * configuration. If the configurations are different or force is `true` Spark context is stopped and
     * started again with the given configuration. */
   def useSparkConf(conf: SparkConf = SparkTemplate.defaultConf, force: Boolean = false): SparkContext = {
-    if (_conf.getAll.toMap != conf.getAll.toMap || force)
+    if (conf == null) {
+      _conf = null
+      if (_sc != null) {
+        _sc.stop()
+        _sc = null
+      }
+    } else if (force || _conf == null || _conf.getAll.toMap != conf.getAll.toMap)
       resetSparkContext(conf)
     _sc
   }
@@ -48,7 +59,8 @@ object SparkTemplate {
       _sc.stop()
     }
 
-    System.err.println("Starting SparkContext with the following configuration:\n" + defaultConf.toDebugString)
+    System.err.println("Starting SparkContext with the following configuration:\n" +
+      defaultConf.toDebugString.lines.map(line => "\t" + line).mkString("\n"))
     _conf = conf.clone()
     for (cp <- sys.env.get("SPARK_SUBMIT_CLASSPATH"))
       conf.setJars(
